f173bed1822cb0bed4259495f56ed4aba85345ad
igordosbruto-star <igordosbruto@gmail.com>
2025-08-17 19:56:46 -0300

    feat: add projeto flag and command stubs

--- Calculadora/src/cli.cpp
+++ Calculadora/src/cli.cpp
@@ -14,6 +14,18 @@ CliOptions parseArgs(int argc, char* argv[]) {
             opt.showHelp = true;
         } else if (a == "--auto") {
             opt.autoMode = true;
+        } else if (a == "--projeto" && i + 1 < argc) {
+            // consome próximo argumento como caminho do projeto
+            opt.projeto = argv[++i];
+        } else if (opt.comando == Comando::Nenhum) {
+            // registra comando principal
+            if (a == "abrir") {
+                opt.comando = Comando::Abrir;
+            } else if (a == "listar") {
+                opt.comando = Comando::Listar;
+            } else if (a == "comparar") {
+                opt.comando = Comando::Comparar;
+            }
         }
     }
     return opt;
